2c7d137b8df2c92bc914e5bc2d48ca1c60621776
birugh <notblue.https@gmail.com>
2025-02-16 17:52:48 +0700

    Membuat static variable untuk Transaksi

--- FTransaksi.cs
+++ FTransaksi.cs
@@ -12,6 +12,11 @@ namespace csharp_lksmart
 {
     public partial class FTransaksi : Form
     {
+        private static string connString = ConfigurationManager.AppSettings["connString"].ToString();
+        private static SqlConnection conn;
+        private static SqlCommand cmd;
+        private static SqlDataAdapter adapter;
+        private DataTable dtKeranjang;
         public FTransaksi()
         {
             InitializeComponent();
