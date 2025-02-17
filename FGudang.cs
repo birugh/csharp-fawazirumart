253b4a3df980f35621dea232396b55b0d0336028
birugh <notblue.https@gmail.com>
2025-02-18 00:14:54 +0700

    Membuat validasi lebih pada saat user ingin menutup form

--- FGudang.cs
+++ FGudang.cs
@@ -199,5 +199,17 @@ namespace csharp_lksmart
         {
             txtSearch.Text = (txtSearch.Text == "") ? "Search by Id" : txtSearch.Text;
         }
+
+        private void FormGudang_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to close?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
     }
 }
