4dc4dad51eadc66a160baea214d4aacef63c48f6
birugh <notblue.https@gmail.com>
2025-02-19 04:52:22 +0700

    Mengganti beberapa penamaan pada design components form, serta mengatur form agar lebih rapih dan mudah digunakan

---
+++ FAdminLaporan.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace csharp_lksmart
+{
+    partial class FAdminLaporan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.btnKelolaLaporan = new System.Windows.Forms.Button();
+            this.btnKelolaUser = new System.Windows.Forms.Button();
+            this.dataGridViewTransaksi = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.chartOmset = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dateStart = new System.Windows.Forms.DateTimePicker();
+            this.dateEnd = new System.Windows.Forms.DateTimePicker();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransaksi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartOmset)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(836, 32);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(49, 13);
+            this.labelTime.TabIndex = 46;
+            this.labelTime.Text = "00:00;00";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(820, 9);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(65, 13);
+            this.labelDate.TabIndex = 45;
+            this.labelDate.Text = "0000/00/00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "Admin";
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(57, 357);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(93, 47);
+            this.btnLog.TabIndex = 42;
+            this.btnLog.Text = "Log Activity";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // btnKelolaLaporan
+            // 
+            this.btnKelolaLaporan.Location = new System.Drawing.Point(57, 280);
+            this.btnKelolaLaporan.Name = "btnKelolaLaporan";
+            this.btnKelolaLaporan.Size = new System.Drawing.Size(93, 47);
+            this.btnKelolaLaporan.TabIndex = 41;
+            this.btnKelolaLaporan.Text = "Kelola Laporan";
+            this.btnKelolaLaporan.UseVisualStyleBackColor = true;
+            this.btnKelolaLaporan.Click += new System.EventHandler(this.btnKelolaLaporan_Click);
+            // 
+            // btnKelolaUser
+            // 
+            this.btnKelolaUser.Location = new System.Drawing.Point(57, 205);
+            this.btnKelolaUser.Name = "btnKelolaUser";
+            this.btnKelolaUser.Size = new System.Drawing.Size(93, 47);
+            this.btnKelolaUser.TabIndex = 40;
+            this.btnKelolaUser.Text = "Kelola User";
+            this.btnKelolaUser.UseVisualStyleBackColor = true;
+            this.btnKelolaUser.Click += new System.EventHandler(this.btnKelolaUser_Click);
+            // 
+            // dataGridViewTransaksi
+            // 
+            this.dataGridViewTransaksi.AllowUserToAddRows = false;
+            this.dataGridViewTransaksi.AllowUserToDeleteRows = false;
+            this.dataGridViewTransaksi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTransaksi.Location = new System.Drawing.Point(217, 144);
+            this.dataGridViewTransaksi.Name = "dataGridViewTransaksi";
+            this.dataGridViewTransaksi.ReadOnly = true;
+            this.dataGridViewTransaksi.Size = new System.Drawing.Size(649, 258);
+            this.dataGridViewTransaksi.TabIndex = 39;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "Kelola Laporan";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(423, 102);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(93, 29);
+            this.btnGenerate.TabIndex = 60;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // chartOmset
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartOmset.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartOmset.Legends.Add(legend1);
+            this.chartOmset.Location = new System.Drawing.Point(217, 425);
+            this.chartOmset.Name = "chartOmset";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartOmset.Series.Add(series1);
+            this.chartOmset.Size = new System.Drawing.Size(649, 276);
+            this.chartOmset.TabIndex = 61;
+            this.chartOmset.Text = "chart1";
+            // 
+            // dateStart
+            // 
+            this.dateStart.Location = new System.Drawing.Point(217, 76);
+            this.dateStart.Name = "dateStart";
+            this.dateStart.Size = new System.Drawing.Size(200, 20);
+            this.dateStart.TabIndex = 62;
+            // 
+            // dateEnd
+            // 
+            this.dateEnd.Location = new System.Drawing.Point(423, 76);
+            this.dateEnd.Name = "dateEnd";
+            this.dateEnd.Size = new System.Drawing.Size(200, 20);
+            this.dateEnd.TabIndex = 63;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(530, 102);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(93, 29);
+            this.btnFilter.TabIndex = 64;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(57, 439);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(93, 47);
+            this.btnLogout.TabIndex = 65;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // FAdminLaporan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 713);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.dateEnd);
+            this.Controls.Add(this.dateStart);
+            this.Controls.Add(this.chartOmset);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.btnKelolaLaporan);
+            this.Controls.Add(this.btnKelolaUser);
+            this.Controls.Add(this.dataGridViewTransaksi);
+            this.Controls.Add(this.label1);
+            this.Name = "FAdminLaporan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form Laporan";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.KelolaFormLaporan_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransaksi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartOmset)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Button btnKelolaLaporan;
+        private System.Windows.Forms.Button btnKelolaUser;
+        private System.Windows.Forms.DataGridView dataGridViewTransaksi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartOmset;
+        private System.Windows.Forms.DateTimePicker dateStart;
+        private System.Windows.Forms.DateTimePicker dateEnd;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
---
+++ FAdminLog.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace csharp_lksmart
+{
+    partial class FormAdminLog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateStart = new System.Windows.Forms.DateTimePicker();
+            this.dateEnd = new System.Windows.Forms.DateTimePicker();
+            this.dataGridViewLogActivity = new System.Windows.Forms.DataGridView();
+            this.btnKelolaUser = new System.Windows.Forms.Button();
+            this.btnKelolaLaporan = new System.Windows.Forms.Button();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLogActivity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Log Activity";
+            // 
+            // dateStart
+            // 
+            this.dateStart.Location = new System.Drawing.Point(192, 149);
+            this.dateStart.Name = "dateStart";
+            this.dateStart.Size = new System.Drawing.Size(200, 20);
+            this.dateStart.TabIndex = 1;
+            // 
+            // dateEnd
+            // 
+            this.dateEnd.Location = new System.Drawing.Point(398, 149);
+            this.dateEnd.Name = "dateEnd";
+            this.dateEnd.Size = new System.Drawing.Size(200, 20);
+            this.dateEnd.TabIndex = 2;
+            // 
+            // dataGridViewLogActivity
+            // 
+            this.dataGridViewLogActivity.AllowUserToAddRows = false;
+            this.dataGridViewLogActivity.AllowUserToDeleteRows = false;
+            this.dataGridViewLogActivity.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLogActivity.Location = new System.Drawing.Point(192, 213);
+            this.dataGridViewLogActivity.Name = "dataGridViewLogActivity";
+            this.dataGridViewLogActivity.ReadOnly = true;
+            this.dataGridViewLogActivity.Size = new System.Drawing.Size(619, 366);
+            this.dataGridViewLogActivity.TabIndex = 4;
+            // 
+            // btnKelolaUser
+            // 
+            this.btnKelolaUser.Location = new System.Drawing.Point(48, 138);
+            this.btnKelolaUser.Name = "btnKelolaUser";
+            this.btnKelolaUser.Size = new System.Drawing.Size(93, 47);
+            this.btnKelolaUser.TabIndex = 5;
+            this.btnKelolaUser.Text = "Kelola User";
+            this.btnKelolaUser.UseVisualStyleBackColor = true;
+            this.btnKelolaUser.Click += new System.EventHandler(this.btnKelolaUser_Click);
+            // 
+            // btnKelolaLaporan
+            // 
+            this.btnKelolaLaporan.Location = new System.Drawing.Point(48, 213);
+            this.btnKelolaLaporan.Name = "btnKelolaLaporan";
+            this.btnKelolaLaporan.Size = new System.Drawing.Size(93, 47);
+            this.btnKelolaLaporan.TabIndex = 6;
+            this.btnKelolaLaporan.Text = "Kelola Laporan";
+            this.btnKelolaLaporan.UseVisualStyleBackColor = true;
+            this.btnKelolaLaporan.Click += new System.EventHandler(this.btnKelolaLaporan_Click);
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(48, 290);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(93, 47);
+            this.btnLog.TabIndex = 7;
+            this.btnLog.Text = "Log Activity";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(505, 179);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(93, 26);
+            this.btnFilter.TabIndex = 8;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Admin";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(763, 9);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(65, 13);
+            this.labelDate.TabIndex = 10;
+            this.labelDate.Text = "0000/00/00";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(779, 32);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(49, 13);
+            this.labelTime.TabIndex = 11;
+            this.labelTime.Text = "00:00:00";
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(48, 423);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(93, 47);
+            this.btnLogout.TabIndex = 12;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(398, 179);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(93, 26);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // FormAdminLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 608);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.btnKelolaLaporan);
+            this.Controls.Add(this.btnKelolaUser);
+            this.Controls.Add(this.dataGridViewLogActivity);
+            this.Controls.Add(this.dateEnd);
+            this.Controls.Add(this.dateStart);
+            this.Controls.Add(this.label1);
+            this.Name = "FormAdminLog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form Log Activity";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLogActivity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateStart;
+        private System.Windows.Forms.DateTimePicker dateEnd;
+        private System.Windows.Forms.DataGridView dataGridViewLogActivity;
+        private System.Windows.Forms.Button btnKelolaUser;
+        private System.Windows.Forms.Button btnKelolaLaporan;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
