a72c5c2118002a8dd5f5ac91fbb5b6ee8f3abf69
birugh <notblue.https@gmail.com>
2025-02-21 08:06:40 +0700

    Membuat button reset agar user dapat dengan cepat mengganti date dengan value tanggal sekarang

--- FAdminLaporan.Designer.cs
+++ FAdminLaporan.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.labelTime = new System.Windows.Forms.Label();
             this.labelDate = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -45,6 +45,7 @@
             this.dateEnd = new System.Windows.Forms.DateTimePicker();
             this.btnFilter = new System.Windows.Forms.Button();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransaksi)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartOmset)).BeginInit();
             this.SuspendLayout();
@@ -138,16 +139,16 @@
             // 
             // chartOmset
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chartOmset.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chartOmset.Legends.Add(legend1);
+            chartArea4.Name = "ChartArea1";
+            this.chartOmset.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chartOmset.Legends.Add(legend4);
             this.chartOmset.Location = new System.Drawing.Point(217, 425);
             this.chartOmset.Name = "chartOmset";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chartOmset.Series.Add(series1);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chartOmset.Series.Add(series4);
             this.chartOmset.Size = new System.Drawing.Size(649, 276);
             this.chartOmset.TabIndex = 61;
             this.chartOmset.Text = "chart1";
@@ -186,11 +187,22 @@
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 29);
+            this.button1.TabIndex = 66;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FAdminLaporan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(897, 713);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnLogout);
             this.Controls.Add(this.btnFilter);
             this.Controls.Add(this.dateEnd);
@@ -231,5 +243,6 @@
         private System.Windows.Forms.DateTimePicker dateEnd;
         private System.Windows.Forms.Button btnFilter;
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button button1;
     }
 }
