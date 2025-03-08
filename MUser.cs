6627ec125b55efab1c4641a53886c201df76a2a7
birugh <notblue.https@gmail.com>
2025-03-08 23:48:43 +0700

    membuat model user

---
+++ MUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lksmart
+{
+    public class MUser
+    {
+        public int id_user { get; set; }
+        public string tipe_user { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string username { get; set; }
+        public string telepon { get; set; }
+        public string password { get; set; }
+        public int is_deleted { get; set; }
+    }
+}
