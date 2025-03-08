3a034f464624083b315347d0814eae2004977330
birugh <notblue.https@gmail.com>
2025-03-08 23:48:59 +0700

    Membuat class khusus untuk connString agar bisa digunakan ulang kodenya.

---
+++ GlobalConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lksmart
+{
+    public class GlobalConfig
+    {
+        private static string connString = ConfigurationManager.AppSettings["connString"];
+        
+        public static string getConnection()
+        {
+            return connString;
+        }
+    }
+}
