fb850668215e9feb8555c24a8eda13f565187992
birugh <notblue.https@gmail.com>
2025-03-09 00:36:14 +0700

    Menghapus using karena tidak digunakan

--- MUser.cs
+++ MUser.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace csharp_lksmart
+﻿namespace csharp_lksmart
 {
     public class MUser
     {
