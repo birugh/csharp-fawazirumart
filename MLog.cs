3ad48fd07795566425a83b9a2a764af444fb42b0
birugh <notblue.https@gmail.com>
2025-03-08 23:48:35 +0700

    Membuat model log

---
+++ MLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lksmart
+{
+    public class MLog
+    {
+        public int id_log { get; set; }
+        public DateTime waktu { get; set; }
+        public string aktivitas { get; set; }
+        public int id_user { get; set; }
+    }
+}
